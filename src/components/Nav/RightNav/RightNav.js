060304e78a437a238fb5d0801fc540e0d723c764
Stephen Parker <stephenparkhum@outlook.com>
2019-11-16 08:38:20 -0800

    Updating the right-nav to add icons

--- src/components/Nav/RightNav/RightNav.js
+++ src/components/Nav/RightNav/RightNav.js
@@ -2,14 +2,22 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import './RightNav.css';
 
+// Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faLaptop } from '@fortawesome/free-solid-svg-icons'
+
 function RightNav() {
+    const aboutIcon = <FontAwesomeIcon icon={faUser} size="2x"/>;
+    const contactIcon = <FontAwesomeIcon icon={faEnvelope} size="2x"/>;
+    const portIcon = <FontAwesomeIcon icon={faLaptop} size="2x"/>;
+
     return (
         <> 
         <nav>
             <ul>
-                <NavLink to="/about" activeClassName="RightNav-active"><li>ABOUT</li></NavLink>
-                <NavLink to="/portfolio" activeClassName="RightNav-active"><li>PORTFOLIO</li></NavLink>
-                <NavLink to="/contact"><li>CONTACT</li></NavLink>
+                <NavLink to="/about" activeClassName="RightNav-active"><li>{aboutIcon}</li></NavLink>
+                <NavLink to="/portfolio" activeClassName="RightNav-active"><li>{portIcon}</li></NavLink>
+                <NavLink to="/contact"><li>{contactIcon}</li></NavLink>
             </ul>
         </nav>
         </>
