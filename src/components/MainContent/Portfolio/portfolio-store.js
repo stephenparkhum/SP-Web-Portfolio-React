319f3d6b1a492fc7e91999d2350709a5f597a0da
Stephen Parker <stephenparkhum@outlook.com>
2019-11-16 08:19:58 -0800

    updating GR 2020 info in portfolio data store

--- src/components/MainContent/Portfolio/portfolio-store.js
+++ src/components/MainContent/Portfolio/portfolio-store.js
@@ -1,6 +1,15 @@
 let portfolioStore = [
     {
         id: 1,
+        title: 'Glossolalia Records (2020)',
+        description: `The 2020 re-design of the Glossolalia Records website. ** Currently in development **`,
+        imgSrc: 'http://drive.google.com/uc?export=view&id=1k-7YsXAD5TgNXgML_aH6iEM0-lMRTq0p',
+        liveLink: 'http://glossolaliarecords',
+        ghLink: 'https://github.com/stephenparkhum/Glossolalia-Records-V2',
+        tags: ['React', 'JavaScript', 'NodeJs', 'mongoDB']
+    },
+    {
+        id: 2,
         title: 'Contour',
         description: `Contour is an app that allows independent artists to plan, route, and book shows/tours! This app is currently in development progress!`,
         imgSrc: 'http://drive.google.com/uc?export=view&id=1oC_jP4HUEpuZknB2x_DqoAtMwM5cLYQX',
@@ -9,7 +18,7 @@ let portfolioStore = [
         tags: ['React', 'JavaScript', 'NodeJs']
     },
     {
-        id: 2,
+        id: 3,
         title: 'genriFy',
         description: `genriFy allows you to search Spotify for a specific genre, and return the top artists.
         This is a query that can help you discover new artists, while also revisiting the ones you
@@ -20,7 +29,7 @@ let portfolioStore = [
         tags: ['JavaScript', 'jQuery', 'Spotify API']
     },
     {
-        id: 3,
+        id: 4,
         title: 'music.query',
         description: `music.Query is an app that allows you to test your music knowledge around rock and heavy metal
         artists. Test your knowledge now and see if you can get a perfect score!`,
