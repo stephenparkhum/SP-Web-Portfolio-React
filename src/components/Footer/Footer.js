a75a5f4aa51f5d23beba7570ebfba93c8e81952d
Stephen HW <sp_dev@stephens-mbp.lan>
2020-05-28 07:35:21 -0700

    Updating skills section on mobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ class ToggleFooter extends Component {
             <>
             <div className="content is-small container has-text-center-mobile has-text-left-desktop">
                 <p>
-                    Website created with <a href="https://bulma.io/">Bulma</a>, <a href="http://reactjs.org">React</a> and copious amounts of coffee <span>//</span> Stephen Parker &copy; {thisYear}
+                    Website created with <a href="https://bulma.io/" rel="noopener noreferrer" target="_blank">Bulma</a>, <a href="http://reactjs.org" rel="noopener noreferrer" target="_blank">React</a> and copious amounts of coffee <span>|</span> Stephen Parker &copy; {thisYear}
                 </p>
             </div>
             </>
--- src/components/Nav/RightNav/RightNav.js
+++ src/components/Nav/RightNav/RightNav.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
 
-// Icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-
 function RightNav() {
     return (
         <> 
