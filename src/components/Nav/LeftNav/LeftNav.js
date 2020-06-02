7c5a548bc9b693c045bed03f3314750ee1f76f0e
Stephen HW <sp_dev@stephens-mbp.lan>
2020-06-01 21:20:36 -0700

    Updating some elements

--- src/components/Nav/LeftNav/LeftNav.js
+++ src/components/Nav/LeftNav/LeftNav.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
 function LeftNav() {
-    let hamburgerBtn = document.getElementsByClassName('navbar-burger');
 
-    let hamburgerClick = (e) => {
-        if (e.classList.contains('is-active')) {
-            e.classList.remove('is-active');
-        } else {
-            e.classList.add('is-active');
+    document.addEventListener('DOMContentLoaded', () => {
+
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+      
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+      
+          // Add a click event on each of them
+          $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+      
+              // Get the target from the "data-target" attribute
+              const target = el.dataset.target;
+              const $target = document.getElementById(target);
+      
+              // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+              el.classList.toggle('is-active');
+              $target.classList.toggle('is-active');
+      
+            });
+          });
         }
-    }
+    });
 
     return (
         <> 
@@ -18,7 +34,7 @@ function LeftNav() {
                 <h1 className="title is-4 has-text-white-ter">Stephen Parker</h1>
             </a>
 
-            <a role="button" className="navbar-burger burger" onClick={hamburgerClick} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
--- src/components/Nav/RightNav/RightNav.js
+++ src/components/Nav/RightNav/RightNav.js
@@ -8,13 +8,13 @@ function RightNav() {
             <div className="navbar-item">
                 <div className="buttons">
                     <a className="button is-link is-normal" href="https://github.com/stephenparkhum" rel="no-follow noreferrer" target="_blank">
-                        <span className="icon is-large">
-                            <i className="fab fa-linkedin-in fa-2x" aria-hidden="true"></i>
+                        <span className="icon is-medium">
+                            <i className="fab fa-linkedin-in fa-lg" aria-hidden="true"></i>
                         </span>
                     </a>
                     <a className="button is-link is-normal" href="https://www.linkedin.com/in/stephenparker-03/" rel="no-follow noreferrer" target="_blank">
-                        <span className="icon is-large">
-                            <i className="fab fa-github fa-2x" aria-hidden="true"></i>
+                        <span className="icon is-medium">
+                            <i className="fab fa-github fa-lg" aria-hidden="true"></i>
                         </span>
                     </a>
                 </div>
