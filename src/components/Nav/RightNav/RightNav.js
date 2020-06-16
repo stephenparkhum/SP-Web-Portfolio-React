3b06220ba2ccba0c3b69e82bd8336377cf536399
Stephen <stephenparkhum@outlook.com>
2020-06-16 11:12:19 -0700

    Fixed navbar bug

--- src/components/Nav/RightNav/RightNav.js
+++ src/components/Nav/RightNav/RightNav.js
@@ -7,12 +7,12 @@ function RightNav() {
         <div className="navbar-end">
             <div className="navbar-item">
                 <div className="buttons">
-                    <a className="button is-link is-normal" href="https://github.com/stephenparkhum" rel="no-follow noreferrer" target="_blank">
+                    <a className="button is-link is-normal" href="https://www.linkedin.com/in/stephenparker-03/" rel="no-follow noreferrer" target="_blank">
                         <span className="icon is-medium">
                             <i className="fab fa-linkedin-in fa-lg" aria-hidden="true"></i>
                         </span>
                     </a>
-                    <a className="button is-link is-normal" href="https://www.linkedin.com/in/stephenparker-03/" rel="no-follow noreferrer" target="_blank">
+                    <a className="button is-link is-normal" href="https://github.com/stephenparkhum" rel="no-follow noreferrer" target="_blank">
                         <span className="icon is-medium">
                             <i className="fab fa-github fa-lg" aria-hidden="true"></i>
                         </span>
