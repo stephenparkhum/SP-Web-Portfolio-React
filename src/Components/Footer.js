02ff43261e2d8cf4f70ef643de1a6fd53583facd
Stephen <stephenparkhum@outlook.com>
2020-11-19 21:41:01 -0800

    Updating styles and such

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,6 +4,7 @@ class Footer extends Component {
   render() {
 
     if(this.props.data){
+      var name = this.props.data.name;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
       })
@@ -22,8 +23,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright {currentYear}</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+            <li>&copy; Copyright {currentYear} | <a href="mailto:stephenparkhum@outlook.com" target="_blank">{name}</a></li>
            </ul>
 
         </div>
